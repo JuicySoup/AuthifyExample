fd450303326cc2825712534095ab78fcc4978d2a
Jonathan Österberg <62814607+JuicySoup@users.noreply.github.com>
2022-01-31 17:08:23 +0100

    Update AuthSettings.h

--- AuthSettings.h
+++ AuthSettings.h
@@ -4,5 +4,5 @@
 namespace AuthSettings {
     const std::string program_key = "x";
     const std::string api_key = "x";
-    const std::string pub_key = "x";
+    const std::string pub_key = "sha256//0NYo0aeAbmNIW39KWcsxgDHmx8++MDco7jxzQN26XdM=";
 }
